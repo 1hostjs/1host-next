a495da24d6c0d564563772d9c3809379e5905518
GrahamSH <grahamshllk@gmail.com>
2021-03-24 13:36:59 +0000

    Init

---
+++ index.js
@@ -0,0 +1,7 @@
+const consola = require('consola')
+
+// See types section for all available types
+
+consola.success('Built!')
+consola.info('Reporter: Some info')
+consola.error('Uh, oh. 1host failed.')
