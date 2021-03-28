fcd674b03d60627de1b5dce78597eaabf5a4e475
William (Wgyt) Horning <williamhorning@wgyt.tk>
2021-03-28 13:31:54 -0400

    L10N FIXES

--- localization/strings.en.js
+++ localization/strings.en.js
@@ -4,6 +4,6 @@ module.exports = {
         "message":"1host.js Message",
         "success":"✔ 1host.js Success",
         "warning":"⚠ 1host.js Warning",
-        "error":"✖ 1host.js Error"
-    
+        "error":"✖ 1host.js Error",
+        "fatalerror":"✖ 1host.js Error"
 }
