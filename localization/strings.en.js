b845d39fe8295b0b92cd157082ebff9f531ad56a
RedGuy12 <paul@reid-family.org>
2021-08-17 10:48:10 -0500

    no .js

--- localization/strings.en.js
+++ localization/strings.en.js
@@ -1,21 +1,21 @@
 import { t } from "@transifex/native";
 t("Starting...");
 t("Yikes, we ran into an error running your project\n");
-t("1host.js Message");
-t("✔ 1host.js Success");
-t("⚠ 1host.js Warning");
-t("✖ 1host.js Error");
-t("✖ 1host.js Fatal Error");
-t("1host.js Help");
+t("1host Message");
+t("✔ 1host Success");
+t("⚠ 1host Warning");
+t("✖ 1host Error");
+t("✖ 1host Fatal Error");
+t("1host Help");
 t('Commands: \n\
-> start: starts 1host.js \n\
+> start: starts 1host \n\
   >> args: directory: changes the directory \n\
   >> usage: 1host start < directory (optional) > \n\
 > config: changes the config file \n\
   >> args: directory: changes the directory \n\
   >> usage: 1host config < directory (optional) > \n
 ');
-t("1host.js Config");
+t("1host Config");
 t("Answer the prompts below");
 t("Choose a port number:");
 t("Do you want to add a module(y/n):");
